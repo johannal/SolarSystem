092bcfb7fc1f5c056e6fe6dba11e270c830dd6fd
Ken Orr <orr@apple.com>
2017-05-16 13:33:15 -0700

    Add a TODO.

--- SolarSystem/Model/PhysicsBody.swift
+++ SolarSystem/Model/PhysicsBody.swift
@@ -9,6 +9,8 @@ import Foundation
 
 public protocol PhysicsBody {
     
+    // TODO come up with things that are functions here rather than properties -- it looks better in the demo.
+    
     var mass: Double { get }
     
     var density: Double { get }
