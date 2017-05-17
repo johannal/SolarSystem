e9d5eafee2f30b409819f8eaa0ce2d5890c6ccf8
Ken Orr <orr@apple.com>
2017-05-16 18:08:19 -0700

    Added some comments.

--- SolarSystem/Model Protocols/OrbitingBody.swift
+++ SolarSystem/Model Protocols/OrbitingBody.swift
@@ -8,14 +8,19 @@
 import Foundation
 import UIKit
 
+/// A body that orbits a primary body. An #OrbitingBody can also be referred to as the secondary body.
 protocol OrbitingBody: PhysicsBody {
     
+    /// Distance of this object from the orbital center point.
     var orbitalRadius: Measurement<UnitLength> { get }
     
+    /// Time taken for a this object to make one complete orbit around the center point.
     var orbitalPeriod: SolarDays { get }
     
+    /// Time it takes to complete one revolution around the axis of rotation.
     var rotationPeriod: SolarDays { get }
     
+    /// Distance between the two farthest points of this objects largest great circle.
     var diameter: Measurement<UnitLength> { get }
     
     /// Get a path describing the orbit of this body around a given center
