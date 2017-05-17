072da8140df888ee09dd3bcce0c52f10d4605456
Sebastian Fischer <sebastian_fischer@apple.com>
2017-05-17 16:47:30 -0700

    Create planets from plist information and let them orbit the sun.

--- SolarSystem/Science/SolarSystemSceneController.swift
+++ SolarSystem/Science/SolarSystemSceneController.swift
@@ -14,12 +14,6 @@ class SolarSystemController: UIViewController {
     
     @IBOutlet weak var solarSystemSceneView: SCNView!
     
-    var sunNode = SCNNode()
-    var sunHaloNode = SCNNode()
-    var earthNode = SCNNode()
-    var earthGroupNode = SCNNode()
-    var moonNode = SCNNode()
-    
     var sceneView = SCNView.init()
     
     override func viewDidLoad() {
@@ -29,6 +23,7 @@ class SolarSystemController: UIViewController {
         
         // Setup HUD
         let sceneHUDController = storyboard!.instantiateViewController(withIdentifier: "sceneHUD")
+        sceneHUDController.view.backgroundColor = UIColor.clear
         addChildViewController(sceneHUDController)
         view.addSubview(sceneHUDController.view)
         
@@ -47,68 +42,72 @@ class SolarSystemController: UIViewController {
     }
     
     private func setupScene() {
-        // Setup scene
-        let scene = SCNScene()
-        solarSystemSceneView.scene = scene
+        let centerNode = solarSystemSceneView.scene?.rootNode.childNode(withName: "SolarSystemCenterNode", recursively: true)!
         
-        // Sun
-        sunNode.geometry = SCNSphere.init(radius: 2.5)
+        let planetInfoPath = Bundle.main.path(forResource: "PlanetDetails", ofType: "plist")!
+        let planetDictionary = NSDictionary.init(contentsOfFile: planetInfoPath)!
         
-        sunNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "2k_sun")
-        sunNode.geometry?.firstMaterial?.diffuse.mipFilter = .linear
-        sunNode.geometry?.firstMaterial?.lightingModel = .constant // no lighting
+        let scaleFactor = 1.0/10000000.0
         
-        sunNode.position = SCNVector3.init(0, 30, 0)
-        scene.rootNode.addChildNode(sunNode)
-        
-        // Earth-rotation (center of rotation of the Earth around the Sun)
-        let earthRotationNode = SCNNode()
-        sunNode.addChildNode(earthRotationNode)
-        
-        // Earth-group (will contain the Earth, and the Moon)
-        earthGroupNode.position = SCNVector3.init(15, 0, 0)
-        earthRotationNode.addChildNode(earthGroupNode)
-        
-        // Earth
-        earthNode.geometry = SCNSphere.init(radius: 1.5)
-        earthNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "2k_earth_daymap")
-        earthNode.geometry?.firstMaterial?.diffuse.mipFilter = .linear
-        earthNode.geometry?.firstMaterial?.lightingModel = .constant // no lighting
-        
-        earthNode.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "2k_earth_specular_map")
-        earthNode.geometry?.firstMaterial?.specular.mipFilter = .linear
-        
-        earthNode.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "2k_earth_normal_map")
-        earthNode.geometry?.firstMaterial?.normal.mipFilter = .linear
-        
-        earthNode.position = SCNVector3.init(0, 0, 0)
-        earthGroupNode.addChildNode(earthNode)
-        
-        // Moon Rotation
-        let moonRotationNode = SCNNode()
-        earthGroupNode.addChildNode(moonRotationNode)
-        
-        // Moon
-        moonNode.geometry = SCNSphere.init(radius: 0.75)
-        
-        moonNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "2k_moon")
-        moonNode.geometry?.firstMaterial?.diffuse.mipFilter = .linear
-        moonNode.geometry?.firstMaterial?.lightingModel = .constant // no lighting
-        
-        moonNode.position = SCNVector3.init(5, 0, 0)
-        moonRotationNode.addChildNode(moonNode)
-        
-        // Add a textured plane to represent Earth's orbit
-        let earthOrbit = SCNNode()
-        earthOrbit.opacity = 0.4;
-        earthOrbit.geometry = SCNPlane.init(width: 31, height: 31)
-        earthOrbit.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "orbit")
-        earthOrbit.geometry?.firstMaterial?.isDoubleSided =  true
-        earthOrbit.geometry?.firstMaterial?.diffuse.mipFilter = .linear
-        earthOrbit.rotation = SCNVector4.init(1, 0, 0, -Double.pi/2)
-        earthOrbit.geometry?.firstMaterial?.lightingModel = .constant // no lighting
-        sunNode.addChildNode(earthOrbit)
-        
+        for (_, value) in planetDictionary {
+            if let planetInfo = value as? Dictionary<String, Any> {
+                // let name = planetInfo["name"]
+                let diameter = planetInfo["diameter"] as! Double
+                let diffuseTexture = planetInfo["diffuseTexture"] as! String
+                let orbitalRadius = planetInfo["orbitalRadius"] as! Double
+                let orbitalPeriod = planetInfo["orbitalPeriod"] as! Double
+                
+                let scaledDiameter = pow(diameter * scaleFactor * 4000.0, (1.0 / 2.0)) // some value to bring up planet size
+                let scaledOrbitalRadius = pow(orbitalRadius * scaleFactor, (1.0 / 2.5)) * 6.4 // / 10.0 // some value to condense the space
+                
+                let planetNode = SCNNode()
+                let planetGeometry = SCNSphere.init(radius: CGFloat(scaledDiameter / 2))
+                
+                let diffuseImage = UIImage(named: diffuseTexture)
+                planetGeometry.firstMaterial?.diffuse.contents = diffuseImage
+                planetGeometry.firstMaterial?.diffuse.mipFilter = .linear
+                planetGeometry.firstMaterial?.lightingModel = .constant // no lighting
+                
+                // Assign normal texture if provided
+                if let normalTexture = planetInfo["normalTexture"] as? String {
+                    planetNode.geometry?.firstMaterial?.normal.contents = UIImage(named: normalTexture)
+                    planetNode.geometry?.firstMaterial?.normal.mipFilter = .linear
+                }
+                
+                // Assign specular texture if provided
+                if let specularTexture = planetInfo["specularTexture"] as? String {
+                    planetNode.geometry?.firstMaterial?.normal.contents = UIImage(named: specularTexture)
+                    planetNode.geometry?.firstMaterial?.normal.mipFilter = .linear
+                }
+                
+                planetNode.geometry = planetGeometry
+                planetNode.position = SCNVector3.init(scaledOrbitalRadius, 0, 0)
+                
+                // Rotation node of the planet
+                let planetRotationNode = SCNNode()
+                planetRotationNode.addChildNode(planetNode)
+                centerNode?.addChildNode(planetRotationNode)
+                
+                // Add orbit
+                let planetOrbit = SCNNode()
+                planetOrbit.opacity = 0.4
+                let orbitSize = CGFloat(scaledOrbitalRadius * 2.0 + scaledDiameter)
+                planetOrbit.geometry = SCNPlane.init(width: orbitSize, height: orbitSize)
+                planetOrbit.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "orbit")
+                planetOrbit.geometry?.firstMaterial?.isDoubleSided =  true
+                planetOrbit.geometry?.firstMaterial?.diffuse.mipFilter = .linear
+                planetOrbit.rotation = SCNVector4.init(1, 0, 0, -Double.pi/2)
+                planetOrbit.geometry?.firstMaterial?.lightingModel = .constant // no lighting
+                centerNode?.addChildNode(planetOrbit)
+                
+                // Add orbiting animation
+                let orbitingAnimation = CABasicAnimation(keyPath: "rotation")
+                orbitingAnimation.duration = orbitalPeriod / 150.0
+                orbitingAnimation.toValue = NSValue(scnVector4: SCNVector4.init(0, 1, 0, Double.pi * 2.0))
+                orbitingAnimation.repeatCount = .greatestFiniteMagnitude
+                planetRotationNode.addAnimation(orbitingAnimation, forKey: "Planet Rotation Animation")
+            }
+        }
     }
     
 }
