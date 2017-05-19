1dd97893e215fbc1e51494b2f2b12e0541ea1031
Sebastian Fischer <sebastian_fischer@apple.com>
2017-05-19 12:24:16 -0700

    Present SpriteKit gravity simulator in landscape mode of planet details controller.

---
+++ SolarSystem/Science/Gravity Simulator/GravitySimulatorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GravitySimulatorViewController.swift
+//  Science
+//
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GravitySimulatorViewController: UIViewController {
+    
+    @IBOutlet weak var gravityView: SKView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        gravityView.scene?.scaleMode = .aspectFit
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+    }
+
+}
---
+++ SolarSystem/Science/Planet Details/PlanetDetailsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  PlanetDetailsViewController.swift
+//  Science
+//
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var additionalDetailsLabel: UILabel!
+    
+    weak var gravitySimulatorVC: GravitySimulatorViewController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.clear
+    }
+    
+    @objc override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        let deviceOrientationIsLandscape = size.width > size.height
+        
+        // Update gravity simulator (present or dismiss)
+        updateGravitySimulator(deviceOrientationIsLandscape)
+    }
+    
+    @IBAction func showPreviousPlanetButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showNextPlanetButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    func updateGravitySimulator(_ orientationIsLandscape: Bool) {
+        if orientationIsLandscape {
+            if gravitySimulatorVC == nil {
+                // Add gravity view controller
+                let gravitySim = storyboard!.instantiateViewController(withIdentifier: "gravitySimulatorVC") as! GravitySimulatorViewController
+                gravitySim.view.translatesAutoresizingMaskIntoConstraints = false
+                addChildViewController(gravitySim)
+                view.addSubview(gravitySim.view)
+                
+                // Constraints
+                gravitySim.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+                gravitySim.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+                gravitySim.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+                gravitySim.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+                
+                gravitySimulatorVC = gravitySim
+            }
+        }
+        else {
+            // Remove gravity view controller
+            gravitySimulatorVC?.view.removeFromSuperview()
+            gravitySimulatorVC?.removeFromParentViewController()
+        }
+    }
+
+}
