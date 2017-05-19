90dc72e125613eb1d6a8610b8225bc1708cd5118
Ken Orr <orr@apple.com>
2017-05-19 08:55:09 -0700

    Add new file that will have a ton of code.

---
+++ SolarSystem/View Classes/SolarSystemView.swift
@@ -0,0 +1,16 @@
+//
+//  SolarSystemView.swift
+//  Science
+//
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+
+// TODO (ken orr): make this file have a TON of code (multiple thousands of lines).
+
+public class SolarSystemView: UIView {
+    
+    
+    
+}
