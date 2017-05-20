417a58b933b71781236dfead8d9739050e995755
Russ Bishop <rbishopjr@apple.com>
2017-05-19 21:00:14 -0700

    Make setupScene() a bit more Swift-y

--- SolarSystem/View Classes/SolarSystemView.swift
+++ SolarSystem/View Classes/SolarSystemView.swift
@@ -27,84 +27,96 @@ public class SolarSystemView: UIView {
     var solarSystemSceneView: SCNView!
     private(set) var planetNodes: [OrbitingBodyNode] = []
     
-    private func setupScene() {
-        let centerNode = solarSystemSceneView.scene?.rootNode.childNode(withName: "SolarSystemCenterNode", recursively: true)!
+    private func setupScene() throws {
+        let centerNode =
+            solarSystemSceneView
+            .scene?
+            .rootNode
+            .childNode(
+                withName: "SolarSystemCenterNode",
+                recursively: true)!
         
         let planetInfoPath = Bundle.main.path(forResource: "PlanetDetails", ofType: "plist")!
         let planetDictionary = NSDictionary.init(contentsOfFile: planetInfoPath)!
         
-        let scaleFactor = 1.0/10000000.0
+        let scaleFactor = 1.0 / 10000000.0
         
-        for (_, value) in planetDictionary {
-            if let planetInfo = value as? Dictionary<String, Any> {
-                let name = planetInfo["name"] as! String
-                let diameter = planetInfo["diameter"] as! Double
-                let diffuseTexture = planetInfo["diffuseTexture"] as! String
-                let orbitalRadius = planetInfo["orbitalRadius"] as! Double
-                
-                let scaledDiameter = pow(diameter * scaleFactor * 40000.0, (1.0 / 2.6)) // increase planet size
-                let scaledOrbitalRadius = pow(orbitalRadius * scaleFactor, (1.0 / 2.5)) * 6.4 // condense the space
-                
-                let planetNode = OrbitingBodyNode()
-                planetNode.bodyInfo = planetInfo
-                planetNode.name = name
-                let planetGeometry = SCNSphere.init(radius: CGFloat(scaledDiameter / 2))
-                
-                let diffuseImage = UIImage(named: diffuseTexture)
-                planetGeometry.firstMaterial?.diffuse.contents = diffuseImage
-                planetGeometry.firstMaterial?.diffuse.mipFilter = .linear
-                planetGeometry.firstMaterial?.lightingModel = .constant // no lighting
-                
-                // Assign normal texture if provided
-                if let normalTexture = planetInfo["normalTexture"] as? String {
-                    planetNode.geometry?.firstMaterial?.normal.contents = UIImage(named: normalTexture)
-                    planetNode.geometry?.firstMaterial?.normal.mipFilter = .linear
-                }
-                
-                // Assign specular texture if provided
-                if let specularTexture = planetInfo["specularTexture"] as? String {
-                    planetNode.geometry?.firstMaterial?.normal.contents = UIImage(named: specularTexture)
-                    planetNode.geometry?.firstMaterial?.normal.mipFilter = .linear
-                }
-                
-                planetNode.geometry = planetGeometry
-                
-                // Rotation node of the planet
-                let planetRotationNode = SCNNode()
-                planetRotationNode.name = name + " Rotation Node"
-                planetNode.rotationNode = planetRotationNode
-                centerNode?.addChildNode(planetRotationNode)
-                
-                // Planet host node
-                let planetHostNode = SCNNode()
-                planetHostNode.name = name + " Host Node"
-                planetHostNode.position = SCNVector3.init(scaledOrbitalRadius, 0, 0)
-                planetRotationNode.addChildNode(planetHostNode)
-                planetHostNode.addChildNode(planetNode)
-                planetNode.solarSystemHostNode = planetHostNode
-                
-                // Add orbit
-                let planetOrbit = SCNNode()
-                planetOrbit.name = name + " Orbit Node"
-                planetOrbit.opacity = 0.4
-                let orbitSize = CGFloat(scaledOrbitalRadius * 2.0 + scaledDiameter / 2.0)
-                planetOrbit.geometry = SCNPlane.init(width: orbitSize, height: orbitSize)
-                planetOrbit.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "orbit")
-                planetOrbit.geometry?.firstMaterial?.isDoubleSided =  true
-                planetOrbit.geometry?.firstMaterial?.diffuse.mipFilter = .linear
-                planetOrbit.rotation = SCNVector4.init(1, 0, 0, -Double.pi/2)
-                planetOrbit.geometry?.firstMaterial?.lightingModel = .constant // no lighting
-                centerNode?.addChildNode(planetOrbit)
-                planetNode.orbitVisualizationNode = planetOrbit
-                
-                // Start orbiting
-                planetNode.startOrbitingAnimation()
-                planetNode.startSpinningAnimation()
-                
-                planetNodes.append(planetNode)
+        for case let (_, planetInfo as Dictionary<String, Any>) in planetDictionary {
+            guard
+                let name = planetInfo["name"] as? String,
+                let diameter = planetInfo["diameter"] as? Double,
+                let diffuseTexture = planetInfo["diffuseTexture"] as? String,
+                let orbitalRadius = planetInfo["orbitalRadius"] as? Double
+            else {
+                throw SolarSystemViewError.invalidData
             }
+            
+            let scaledDiameter = pow(diameter * scaleFactor * 40000.0, (1.0 / 2.6)) // increase planet size
+            let scaledOrbitalRadius = pow(orbitalRadius * scaleFactor, (1.0 / 2.5)) * 6.4 // condense the space
+            
+            let planetNode = OrbitingBodyNode()
+            planetNode.bodyInfo = planetInfo
+            planetNode.name = name
+            let planetGeometry = SCNSphere.init(radius: CGFloat(scaledDiameter / 2))
+            
+            let diffuseImage = UIImage(named: diffuseTexture)
+            planetGeometry.firstMaterial?.diffuse.contents = diffuseImage
+            planetGeometry.firstMaterial?.diffuse.mipFilter = .linear
+            planetGeometry.firstMaterial?.lightingModel = .constant // no lighting
+            
+            // Assign normal texture if provided
+            if let normalTexture = planetInfo["normalTexture"] as? String {
+                planetNode.geometry?.firstMaterial?.normal.contents = UIImage(named: normalTexture)
+                planetNode.geometry?.firstMaterial?.normal.mipFilter = .linear
+            }
+            
+            // Assign specular texture if provided
+            if let specularTexture = planetInfo["specularTexture"] as? String {
+                planetNode.geometry?.firstMaterial?.normal.contents = UIImage(named: specularTexture)
+                planetNode.geometry?.firstMaterial?.normal.mipFilter = .linear
+            }
+            
+            planetNode.geometry = planetGeometry
+            
+            // Rotation node of the planet
+            let planetRotationNode = SCNNode()
+            planetRotationNode.name = name + " Rotation Node"
+            planetNode.rotationNode = planetRotationNode
+            centerNode?.addChildNode(planetRotationNode)
+            
+            // Planet host node
+            let planetHostNode = SCNNode()
+            planetHostNode.name = name + " Host Node"
+            planetHostNode.position = SCNVector3.init(scaledOrbitalRadius, 0, 0)
+            planetRotationNode.addChildNode(planetHostNode)
+            planetHostNode.addChildNode(planetNode)
+            planetNode.solarSystemHostNode = planetHostNode
+            
+            // Add orbit
+            let planetOrbit = SCNNode()
+            planetOrbit.name = name + " Orbit Node"
+            planetOrbit.opacity = 0.4
+            let orbitSize = CGFloat(scaledOrbitalRadius * 2.0 + scaledDiameter / 2.0)
+            planetOrbit.geometry = SCNPlane.init(width: orbitSize, height: orbitSize)
+            planetOrbit.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "orbit")
+            planetOrbit.geometry?.firstMaterial?.isDoubleSided =  true
+            planetOrbit.geometry?.firstMaterial?.diffuse.mipFilter = .linear
+            planetOrbit.rotation = SCNVector4.init(1, 0, 0, -Double.pi/2)
+            planetOrbit.geometry?.firstMaterial?.lightingModel = .constant // no lighting
+            centerNode?.addChildNode(planetOrbit)
+            planetNode.orbitVisualizationNode = planetOrbit
+            
+            // Start orbiting
+            planetNode.startOrbitingAnimation()
+            planetNode.startSpinningAnimation()
+            
+            planetNodes.append(planetNode)
         }
     }
+}
+
+enum SolarSystemViewError: Error {
+    case invalidData
 }
 
 public struct Bounds {
