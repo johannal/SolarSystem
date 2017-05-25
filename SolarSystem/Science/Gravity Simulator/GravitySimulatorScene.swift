41d7cddd91d2711cce9c89744c0900fc5bdce7a1
Sebastian Fischer <sebastian_fischer@apple.com>
2017-05-25 12:05:39 -0700

    Rename SpriteKit nodes.

--- SolarSystem/Science/Gravity Simulator/GravitySimulatorScene.swift
+++ SolarSystem/Science/Gravity Simulator/GravitySimulatorScene.swift
@@ -25,39 +25,39 @@ class GravitySimulatorScene: SKScene {
     
     override func didChangeSize(_ oldSize: CGSize) {
         // Update positioning of walls
-        if let leftWall = childNode(withName: "leftWall") as? SKSpriteNode {
+        if let leftWall = childNode(withName: "Left Edge") as? SKSpriteNode {
             leftWall.size = CGSize.init(width: wallThickness, height: size.height)
             leftWall.position = CGPoint.init(x: -size.width/2.0 - wallThickness/2.0, y: 0)
             leftWall.updateStaticPhysicsBody()
         }
         
-        if let rightWall = childNode(withName: "rightWall") as? SKSpriteNode {
+        if let rightWall = childNode(withName: "Right Edge") as? SKSpriteNode {
             rightWall.size = CGSize.init(width: wallThickness, height: size.height)
             rightWall.position = CGPoint.init(x: size.width/2.0 + wallThickness/2.0, y: 0)
             rightWall.updateStaticPhysicsBody()
         }
         
         let horizontalWallLength = size.width + wallThickness * 2.0
-        if let topWall = childNode(withName: "topWall") as? SKSpriteNode {
+        if let topWall = childNode(withName: "Ceiling") as? SKSpriteNode {
             topWall.size = CGSize.init(width: horizontalWallLength, height: wallThickness)
             topWall.position = CGPoint.init(x: 0, y: size.height/2.0 + wallThickness/2.0)
             topWall.updateStaticPhysicsBody()
         }
         
-        if let bottomWall = childNode(withName: "bottomWall") as? SKSpriteNode {
+        if let bottomWall = childNode(withName: "Floor") as? SKSpriteNode {
             bottomWall.size = CGSize.init(width: horizontalWallLength, height: wallThickness)
             bottomWall.position = CGPoint.init(x: 0, y: -size.height/2.0 - wallThickness/2.0)
             bottomWall.updateStaticPhysicsBody()
         }
         
-        if let backgroundNode = self.childNode(withName: "backgroundNode") as? SKSpriteNode {
+        if let backgroundNode = self.childNode(withName: "Background") as? SKSpriteNode {
             backgroundNode.size = CGSize.init(width: size.height/3*4, height: size.height)
             backgroundNode.position = .zero
         }
     }
     
     func simulateZeroGravity() {
-        if let elementHostNode = childNode(withName: "ElementHost") {
+        if let elementHostNode = childNode(withName: "Element Host") {
             for elementNode in elementHostNode.children {
                 if let node = elementNode as? SKSpriteNode {
                     node.physicsBody?.affectedByGravity = true
@@ -68,7 +68,7 @@ class GravitySimulatorScene: SKScene {
             }
         }
         
-        if let noiseFieldHostNode = childNode(withName: "noiseFieldHostNode") {
+        if let noiseFieldHostNode = childNode(withName: "Noise Field Host") {
             for noiseFieldNode in noiseFieldHostNode.children {
                 if let node = noiseFieldNode as? SKFieldNode {
                     node.isEnabled = true
@@ -80,7 +80,7 @@ class GravitySimulatorScene: SKScene {
     }
     
     func activateGravity(_ gravity: Double) {
-        if let noiseFieldHostNode = childNode(withName: "noiseFieldHostNode") {
+        if let noiseFieldHostNode = childNode(withName: "Noise Field Host") {
             for noiseFieldNode in noiseFieldHostNode.children {
                 if let node = noiseFieldNode as? SKFieldNode {
                     node.isEnabled = false
