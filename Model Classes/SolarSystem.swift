a7654b3753833d0c2cc00bdaa9e71e9735d7d536
Ken Orr <orr@apple.com>
2017-05-27 12:42:44 -0700

    Remove unneeded type name.

--- Model Classes/SolarSystem.swift
+++ Model Classes/SolarSystem.swift
@@ -14,14 +14,14 @@ public class SolarSystem {
     let sun = Star(name: "Sun", color: #colorLiteral(red: 1, green: 0.5549516082, blue: 0.005077361129, alpha: 1))
     
     // Create the planets.
-    let mercury: Planet = Planet(name: "Mercury", color: #colorLiteral(red: 0.6472379565, green: 0.6025107503, blue: 0.5466887951, alpha: 1))
-    let venus: Planet = Planet(name: "Venus", color: #colorLiteral(red: 0.3882164955, green: 0.3436552286, blue: 0.2652897239, alpha: 1))
-    let earth: Planet = Planet(name: "Earth", color: #colorLiteral(red: 0.3583836854, green: 0.4615976214, blue: 0.5621570349, alpha: 1))
-    let mars: Planet = Planet(name: "Mars", color: #colorLiteral(red: 0.8524389267, green: 0.6224156618, blue: 0.4205289483, alpha: 1))
-    let jupiter: Planet = Planet(name: "Jupiter", color: #colorLiteral(red: 0.398963064, green: 0.4041557312, blue: 0.36931777, alpha: 1))
-    let saturn: Planet = Planet(name: "Saturn", color: #colorLiteral(red: 0.8856078386, green: 0.7523123622, blue: 0.5600671172, alpha: 1))
-    let uranus: Planet = Planet(name: "Uranus", color: #colorLiteral(red: 0.5745319724, green: 0.7137514353, blue: 0.7544075847, alpha: 1))
-    let neptune: Planet = Planet(name: "Neptune", color: #colorLiteral(red: 0.3400839567, green: 0.5141240954, blue: 0.6517244577, alpha: 1))
+    let mercury = Planet(name: "Mercury", color: #colorLiteral(red: 0.6472379565, green: 0.6025107503, blue: 0.5466887951, alpha: 1))
+    let venus = Planet(name: "Venus", color: #colorLiteral(red: 0.3882164955, green: 0.3436552286, blue: 0.2652897239, alpha: 1))
+    let earth = Planet(name: "Earth", color: #colorLiteral(red: 0.3583836854, green: 0.4615976214, blue: 0.5621570349, alpha: 1))
+    let mars = Planet(name: "Mars", color: #colorLiteral(red: 0.8524389267, green: 0.6224156618, blue: 0.4205289483, alpha: 1))
+    let jupiter = Planet(name: "Jupiter", color: #colorLiteral(red: 0.398963064, green: 0.4041557312, blue: 0.36931777, alpha: 1))
+    let saturn = Planet(name: "Saturn", color: #colorLiteral(red: 0.8856078386, green: 0.7523123622, blue: 0.5600671172, alpha: 1))
+    let uranus = Planet(name: "Uranus", color: #colorLiteral(red: 0.5745319724, green: 0.7137514353, blue: 0.7544075847, alpha: 1))
+    let neptune = Planet(name: "Neptune", color: #colorLiteral(red: 0.3400839567, green: 0.5141240954, blue: 0.6517244577, alpha: 1))
     
     // Create arrays for the objects in our SolarSystem.
     let planets: [Planet]
