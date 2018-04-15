dc8baaa5bfc4899118a60ea9cd4bac2355df6f21
Sebastian Fischer <sebastian_fischer@apple.com>
2018-04-15 12:51:30 -0700

    Use earth-at-night texture in inspector in dark mode.

--- App/Solar System Mac/Inspector.swift
+++ App/Solar System Mac/Inspector.swift
@@ -10,21 +10,36 @@ import Cocoa
 import SceneKit
 
 class Inspector: NSViewController {
-    
     @IBOutlet weak var sceneView: SCNView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        // Observe effective appearance changes
+        addObserver(self, forKeyPath: #keyPath(view.effectiveAppearance), options: [.new], context: nil)
     }
     
-    // TODO: Switch between earth and earth at night texture based on appearance
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if (keyPath == #keyPath(view.effectiveAppearance)) {
+            effectiveAppearanceDidChange()
+        }
+    }
+    
     func effectiveAppearanceDidChange() {
+        let effectiveAppearanceIsDark = view.effectiveAppearance._isDark
+        
         if let astronomicalObjectNode = sceneView.scene?.rootNode.childNode(withName: "astronomicalObject", recursively: true) {
-            let textureName = "Earth" // EarthNightLights
+            let textureName = effectiveAppearanceIsDark ? "EarthNightLights" : "Earth"
             let image = NSImage(named: NSImage.Name(rawValue: textureName))
             astronomicalObjectNode.geometry?.firstMaterial?.diffuse.contents = image
         }
     }
     
+}
+
+extension NSAppearance {
+    var _isDark: Bool {
+        return name.rawValue.contains("Dark")
+    }
 }
