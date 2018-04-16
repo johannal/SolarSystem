96a2e49e35779a1c18785b64e62f1cbc9fe36127
Andrew Nesbitt <nesbitt@apple.com>
2018-04-16 11:57:29 -0700

    Updating script

---
+++ App/Solar System Mac/Unit Tests/MoonTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoonTests.swift
+//
+//  Created by Andrew Nesbitt on 4/16/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import XCTest
+
+class MoonTests: XCTestCase {
+    
+    func testExample() {
+        
+    }
+    
+}
---
+++ App/Solar System Mac/Unit Tests/PlanetTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetTests.swift
+//
+//  Created by Andrew Nesbitt on 4/16/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import XCTest
+
+class PlanetTests: XCTestCase {
+    
+    func testExample() {
+        
+    }
+    
+}
