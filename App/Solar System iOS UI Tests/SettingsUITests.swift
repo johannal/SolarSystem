1c7d104fc14cca2df03261b04cfe20daf05a3890
Ethan Vaughan <evaughan@apple.com>
2018-05-10 17:33:13 -0700

    Add a settings view controller with UI tests

---
+++ App/Solar System iOS UI Tests/SettingsUITests.swift
@@ -0,0 +1,44 @@
+//
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import XCTest
+
+class SettingsUITests: XCTestCase {
+
+    func testSettings() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        app.buttons["Settings"].tap()
+        
+        app.cells.containing(.staticText, identifier: "Simulate gravity").switches.element.tap()
+        
+        app.cells.containing(.staticText, identifier: "Gravity mode").segmentedControls.buttons["Moon"].tap()
+        
+        app.cells.containing(.staticText, identifier: "Show dwarf planets").switches.element.tap()
+        
+        let radiusSlider = app.cells.containing(.staticText, identifier: "Visible radius (au)").sliders.element
+        
+        let minValue = 1
+        let maxValue = 35
+        let strideValue = 5
+        
+        var lastValue = minValue
+        for i in stride(from: minValue, through: maxValue, by: strideValue) {
+            let startingCoordinate = radiusSlider.coordinate(withNormalizedOffset: CGVector(dx: CGFloat(lastValue)/CGFloat(maxValue), dy: 0.5))
+            let targetCoordinate = radiusSlider.coordinate(withNormalizedOffset: CGVector(dx: CGFloat(i)/CGFloat(maxValue), dy: 0.5))
+            
+            startingCoordinate.press(forDuration: 1, thenDragTo: targetCoordinate)
+            
+            lastValue = i
+        }
+        
+        app.cells.containing(.staticText, identifier: "Distance units").textFields.element.tap()
+        
+        for _ in 0 ..< 2 {
+             app.pickerWheels.element.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.6)).tap()
+        }
+    }
+}
---
+++ App/Solar System/Settings/SettingsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    @IBOutlet weak var distanceUnitsTextField: UITextField!
+    
+    let distanceUnits = ["Astronomical units (au)", "Kilometers (km)", "Meters (m)"]
+    
+    lazy var unitsPickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        return pickerView
+    }()
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.distanceUnitsTextField.inputView = unitsPickerView
+        
+        let initialRow = 0
+        self.distanceUnitsTextField.text = self.distanceUnits[initialRow]
+        self.unitsPickerView.selectRow(initialRow, inComponent: 0, animated: false)
+    }
+    
+    // MARK: - Picker view data source & delegate
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return distanceUnits.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return distanceUnits[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.distanceUnitsTextField.text = self.distanceUnits[row]
+    }
+}
+
+class NoCaretTextField: UITextField {
+    override func caretRect(for position: UITextPosition) -> CGRect {
+        return CGRect.zero
+    }
+}
