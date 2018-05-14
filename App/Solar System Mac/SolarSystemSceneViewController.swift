31b3acf8c98718ad44e9f4b560f7928599460023
Ken Orr <orr@apple.com>
2018-05-14 10:27:40 -0700

    Return an optional color so we don't need to type the "!".

--- App/Solar System Mac/SolarSystemSceneViewController.swift
+++ App/Solar System Mac/SolarSystemSceneViewController.swift
@@ -43,7 +43,7 @@ class SolarSystemSceneViewController: NSViewController, SolarSystemSceneControll
     /// Duis id interdum elit. Suspendisse id risus massa. In eu sollicitudin enim, non fringilla nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consectetur, nulla ac hendrerit pharetra, eros orci consectetur sapien, a consequat nisi elit vel ex. Pellentesque ultrices felis et urna scelerisque efficitur. Vivamus scelerisque mattis tellus, non porta libero interdum et. Curabitur varius sagittis enim id dignissim.
     ///
     /// - Returns: The color to use to draw an #OrbitingBodyNode's orbit path.
-    func orbitPathColor() -> NSColor {
+    func orbitPathColor() -> NSColor? {
         return NSColor(red: 0.34, green: 0.532, blue: 0.541, alpha: 0.75)
     }
     
@@ -51,7 +51,7 @@ class SolarSystemSceneViewController: NSViewController, SolarSystemSceneControll
     /// Ut sed suscipit lacus, in fermentum lectus. Donec varius consequat pellentesque. Maecenas ullamcorper nisi sed dolor pellentesque, id sagittis turpis suscipit. Nam hendrerit vestibulum tincidunt. Etiam a magna gravida, iaculis quam ut, lacinia justo. Phasellus ex tortor, dapibus in finibus eu, dictum at libero. Pellentesque et dignissim velit, id rutrum erat. Nam molestie justo neque, ut posuere tortor malesuada non. Quisque sit amet facilisis enim. Morbi elementum lacus ac massa ultricies, et convallis velit dignissim. Ut rhoncus justo non nibh placerat pharetra. Vestibulum volutpat varius tellus non auctor. Suspendisse tempus neque quis porttitor tristique. Duis tristique tortor in quam ornare eleifend. In sed risus dolor.
     ///
     /// - Returns: The color to use to draw a selected #OrbitingBodyNode's orbit path.
-    func orbitSelectedPathColor() -> NSColor {
+    func orbitSelectedPathColor() -> NSColor? {
         return NSColor(red: 0.28, green: 0.49, blue: 0.14, alpha: 0.9)
     }
     
@@ -59,7 +59,7 @@ class SolarSystemSceneViewController: NSViewController, SolarSystemSceneControll
     /// Duis ullamcorper est ante, eget elementum tortor interdum et. Quisque pellentesque commodo libero, at vestibulum arcu. Morbi ullamcorper lorem ut neque blandit pretium. Nullam lectus justo, sagittis nec egestas ut, rutrum vel lacus. Duis et metus nec nisl egestas dignissim. Aliquam a accumsan nunc, a tincidunt augue. Donec sollicitudin aliquet ornare. Aliquam volutpat at sem quis bibendum. Vestibulum volutpat justo vel nisl volutpat aliquam. Sed consequat, lacus lacinia fermentum dapibus, mi turpis hendrerit odio, ut pharetra enim lorem eget eros. Sed vitae odio tristique nibh dictum laoreet. Quisque scelerisque, quam at volutpat auctor, mi ante mollis massa, sit amet gravida odio mauris non dolor. Sed finibus elit et dolor bibendum dignissim.
     ///
     /// - Returns: The color to use to draw a halo under an #OrbitingBodyNode's orbit path.
-    func orbitHaloColor() -> NSColor {
+    func orbitHaloColor() -> NSColor? {
         return NSColor(red: 0.74, green: 0.74, blue: 1.0, alpha: 0.3)
     }
     
