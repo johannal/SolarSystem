67f12a8330e2a9e543d291aee307507d13f81dcd
Andrew Nesbitt <nesbitt@apple.com>
2018-05-15 07:00:45 -0700

    Adding script to pre-boot and layout simulators in a seperate space for the demo

---
+++ AndrewBootSimulators.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+username=$(whoami)
+echo "Username is $username"
+
+simcount=3
+xcode="/Volumes/Justice/Xcode.app"
+echo "Will boot $simcount simulators using $xcode"
+
+# echo "Shutting down existing simulators"
+# xcrun simctl --set "/Users/$username/Library/Developer/XCTestDevices" shutdown all
+
+echo "Booting simulators"
+XCODE_DEVELOPER_DIR="$xcode/Contents/Developer" ./sotubootsims --device-name 'iPhone X' --count $simcount
+
+echo "Listing simulators after boot"
+xcrun simctl --set "/Users/$username/Library/Developer/XCTestDevices" list
+
+echo "Done!"
