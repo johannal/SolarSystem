fafc66d0e5f198b79f5c9c5a3c3fe7b9e4a8f27c
Andrew Nesbitt <nesbitt@apple.com>
2018-05-15 15:04:12 -0700

    Use xcode-select to drive the active Xcode

--- AndrewBootSimulators.sh
+++ AndrewBootSimulators.sh
@@ -4,14 +4,14 @@ username=$(whoami)
 echo "Username is $username"
 
 simcount=4
-xcode="/Volumes/Justice/Xcode.app"
+xcode=$(xcode-select -p)
 echo "Will boot $simcount simulators using $xcode"
 
 # echo "Shutting down existing simulators"
 # xcrun simctl --set "/Users/$username/Library/Developer/XCTestDevices" shutdown all
 
 echo "Booting simulators"
-XCODE_DEVELOPER_DIR="$xcode/Contents/Developer" ./sotubootsims --device-name 'iPhone X' --count $simcount
+XCODE_DEVELOPER_DIR="$xcode" ./sotubootsims --device-name 'iPhone X' --count $simcount
 
 echo "Listing simulators after boot"
 xcrun simctl --set "/Users/$username/Library/Developer/XCTestDevices" list
