adfd75a48db6809c67d8b955792cfa827702601d
Andrew Nesbitt <nesbitt@apple.com>
2018-05-15 15:54:33 -0700

    Use iPhone 8 as a workaround for UIKit size bug for now

--- AndrewBootSimulators.sh
+++ AndrewBootSimulators.sh
@@ -7,11 +7,12 @@ simcount=4
 xcode=$(xcode-select -p)
 echo "Will boot $simcount simulators using $xcode"
 
-# echo "Shutting down existing simulators"
-# xcrun simctl --set "/Users/$username/Library/Developer/XCTestDevices" shutdown all
+echo "Shutting down existing simulators"
+xcrun simctl --set "/Users/$username/Library/Developer/XCTestDevices" shutdown all
+rm -rf "/Users/$username/Library/Developer/XCTestDevices"
 
 echo "Booting simulators"
-XCODE_DEVELOPER_DIR="$xcode" ./sotubootsims --device-name 'iPhone X' --count $simcount
+XCODE_DEVELOPER_DIR="$xcode" ./sotubootsims --device-name 'iPhone 8' --count $simcount
 
 echo "Listing simulators after boot"
 xcrun simctl --set "/Users/$username/Library/Developer/XCTestDevices" list
