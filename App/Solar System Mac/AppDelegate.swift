e67f45efd9de07778ac295070798e7726a71fe2e
Sebastian Fischer <sebastian_fischer@apple.com>
2018-05-21 11:53:05 -0700

    Register default defaults.

--- App/Solar System Mac/AppDelegate.swift
+++ App/Solar System Mac/AppDelegate.swift
@@ -12,6 +12,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         _showDockIconIfNecessary()
+        
+        // Register default defaults
+        UserDefaults.standard.register(defaults: ["AutomaticNewsFeedUpdates" : false, "DisplayLinkAnimations" : false])
     }
     
     fileprivate func _showDockIconIfNecessary() {
