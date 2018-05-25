daa6a9860860377d9ee03376302415d808c485a2
Daniel Delwood <ddelwood@apple.com>
2018-05-24 21:52:04 -0700

    Implementing tracing with slightly more detail

--- App/Shared/Networking/Primitives/AdditionalSignposts.swift
+++ App/Shared/Networking/Primitives/AdditionalSignposts.swift
@@ -6,8 +6,29 @@
 //
 
 import Foundation
+import _SwiftOSOverlayShims
 import os.signpost
 
-public func os_signpost(_ type: OSSignpostType, log: OSLog, name: StaticString, signpostID: OSSignpostID, _ format: StaticString, _ arguments: CVarArg...) {
-    os_signpost(type: type, log: log, name: name, signpostID: signpostID, format, arguments)
+public func os_signpost(_ type: OSSignpostType, dso: UnsafeRawPointer = #dsohandle, log: OSLog, name: StaticString, signpostID: OSSignpostID = .exclusive, _ format: StaticString, _ arguments: CVarArg...) {
+    guard log.signpostsEnabled else { return }
+    let ra = _swift_os_log_return_address()
+    name.withUTF8Buffer { (nameBuf: UnsafeBufferPointer<UInt8>) in
+        // Since dladdr is in libc, it is safe to unsafeBitCast
+        // the cstring argument type.
+        nameBuf.baseAddress!.withMemoryRebound(
+            to: CChar.self, capacity: nameBuf.count
+        ) { nameStr in
+            format.withUTF8Buffer { (formatBuf: UnsafeBufferPointer<UInt8>) in
+                // Since dladdr is in libc, it is safe to unsafeBitCast
+                // the cstring argument type.
+                formatBuf.baseAddress!.withMemoryRebound(
+                    to: CChar.self, capacity: formatBuf.count
+                ) { formatStr in
+                    withVaList(arguments) { valist in
+                        _swift_os_signpost_with_format(dso, ra, log, type, nameStr, signpostID.rawValue, formatStr, valist)
+                    }
+                }
+            }
+        }
+    }
 }
