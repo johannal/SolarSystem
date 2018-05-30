65edd973fd0c90ae8b0407571858ebffd530b283
Ken Orr <orr@apple.com>
2018-05-30 15:12:47 -0700

    Update to use new PDF art.

--- SOTU-Ken.sh
+++ SOTU-Ken.sh
@@ -1,2 +1,2 @@
 sudo mkdir -p /WWDC18/
-sudo cp 'App/Instruments/Solar Networking/SolarSystemTemplate.png' /WWDC18/SolarSystemTemplate.png
+sudo cp 'App/Instruments/Solar Networking/SolarSystemTemplate.pdf' /WWDC18/SolarSystemTemplate.pdf
